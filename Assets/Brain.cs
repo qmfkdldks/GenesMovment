90cc9c60793d81e0c2b73110243619415e4e7deb
Brian Kang <qmfkdldks@gmail.com>
2019-11-25 23:24:49 -0300

    initial commit

---
+++ Assets/Brain.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (SimpleCharacterControl))]
+public class Brain : MonoBehaviour
+{
+    public int DNALength = 1;
+    public float timeAlive;
+    public DNA dna;
+
+    private SimpleCharacterControl m_Character;
+    private Vector3 m_Move;
+    private bool m_Jump;
+    bool alive = true;
+
+    void OnCollisionEnter(Collision obj)
+    {
+        if(obj.gameObject.tag == "dead")
+        {
+            alive = false;
+        }
+    }
+
+    public void Init()
+    {
+        dna = new DNA(DNALength, 6);
+        m_Character = GetComponent<SimpleCharacterControl>();
+        timeAlive = 0;
+        alive = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float h = 0;
+        float v = 0;
+        bool crouch = false;
+        if(dna.GetGene(0) == 0) v = 1;
+        else if(dna.GetGene(0) == 1) v = -1;
+        else if(dna.GetGene(0) == 2) h = -1;
+        else if(dna.GetGene(0) == 3) h = 1;
+        else if(dna.GetGene(0) == 4) m_Jump = true;
+        else if(dna.GetGene(0) == 5) crouch = true;
+    
+        m_Character.Move(v, h, m_Jump);
+        m_Jump = false;
+        if(alive)
+            timeAlive += Time.deltaTime;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # GenesMovment
 Unity project which simulate basic genetic algorithms
+It simulates process of natural selection. Fittest individuals from a population will survive longer and they produce offspring which inherit the characteristics of the parents and will be added to the next generation.
+
+Is designed to consider five important phases in genetic algorithm
+1. Initial population
+2. Fitness function
+3. Selection
+4. Crossover
+5. Mutation
+
+- C#
+- Genetic Algorithm
